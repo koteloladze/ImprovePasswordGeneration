038f3773aef1c8127d4f8ae963b6b094bf132ae4
Konstantine Loladze <Konstantine_Loladze@epam.com>
2022-11-24 20:49:16 +0400

    Add project files.

---
+++ ImprovePasswordGeneration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+{
+
+    var iterate = 10000;
+    var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+    byte[] hash = pbkdf2.GetBytes(20);
+
+    byte[] hashBytes = new byte[36];
+    Array.Copy(salt, 0, hashBytes, 0, 16);
+    Array.Copy(hash, 0, hashBytes, 16, 20);
+
+    var passwordHash = Convert.ToBase64String(hashBytes);
+
+    return passwordHash;
+}
